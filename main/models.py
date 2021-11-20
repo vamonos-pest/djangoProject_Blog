2421e8ce09b7fc67c5a1a43629309981e167b6fc
vamonos-pest <luchinskiye@gmail.com>
2021-11-20 23:36:19 +0300

    Create model Post

--- main/models.py
+++ main/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField('Тема', max_length=255)
     post = models.TextField('Описание')
+
+    def __str__(self):
+        return self.title
